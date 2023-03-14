e39ec682ee0c705b2f07cd6d97451c0009b41d43
Ollie Munday <olliemunday@gmail.com>
2023-03-14 22:56:51 +0000

    Initial Commit

---
+++ myairops/myairopsApp.swift
@@ -0,0 +1,17 @@
+//
+//  myairopsApp.swift
+//  myairops
+//
+//  Created by Ollie on 14/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct myairopsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
